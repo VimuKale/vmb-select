291acea3f10cb45144e79a8d89589c4904a7b30c
Vimu-Kale <vimurk111@gmail.com>
2023-07-07 15:33:23 +0530

    all pushed

---
+++ dist/App.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _CustomSelect = _interopRequireDefault(require("./components/CustomSelect"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function App() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(_CustomSelect.default, null));
+}
+var _default = App;
+exports.default = _default;
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "CustomSelect", {
+  enumerable: true,
+  get: function get() {
+    return _CustomSelect.default;
+  }
+});
+var _CustomSelect = _interopRequireDefault(require("./components/CustomSelect"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
